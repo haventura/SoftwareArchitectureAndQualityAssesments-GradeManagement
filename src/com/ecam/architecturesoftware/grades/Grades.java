34877f4e0570bc53290d87c90eead0650748cb4b
haventura <60008966+haventura@users.noreply.github.com>
2021-11-19 08:51:08 +0100

    sync test

--- src/com/ecam/architecturesoftware/grades/Grades.java
+++ src/com/ecam/architecturesoftware/grades/Grades.java
@@ -3,6 +3,6 @@ package com.ecam.architecturesoftware.grades;
 public class Grades
 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 }
