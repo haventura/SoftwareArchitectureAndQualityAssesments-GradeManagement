ed800e47c34ccb921dc4f2b8c8685fa7f95b0622
DaveTheDudeFromECAM <20194@ecam.be>
2021-12-10 10:40:46 +0100

    changed data type from long to string for teacher

--- src/main/java/com/ecam/architecturesoftware/gestionnotes/GradeNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ecam.architecturesoftware.gestionnotes;
-
-public class GradeNotFoundException extends RuntimeException {
-
-    GradeNotFoundException(Long id) {
-        super("Could not find teacher " + id);
-    }
-}
